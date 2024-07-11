5f972becc8c6e965ec6a5c5b7ae9528cf5bc135f
zenith110 <thequake@live.com>
2024-07-11 11:09:09 -0400

    Added destinction between server + client

--- models/models-mystery-gift-toml.go
+++ models/models-mystery-gift-toml.go
@@ -1,13 +1,13 @@
 package models
 
-type MysteryGift struct {
-	Name          string      `toml:"name"`
-	GiftType      string      `toml:"giftType"`
-	Pokemon       PokemonGift `toml:"pokemonGift"`
-	BeginningDate string      `toml:"beginningDate"`
-	EndDate       string      `toml:"endDate"`
+type MysteryGiftClient struct {
+	Name          string            `toml:"name"`
+	GiftType      string            `toml:"giftType"`
+	Pokemon       PokemonGiftClient `toml:"pokemonGift"`
+	BeginningDate string            `toml:"beginningDate"`
+	EndDate       string            `toml:"endDate"`
 }
-type PokemonGift struct {
+type PokemonGiftClient struct {
 	OriginalTrainer  string      `toml:"originalTrainer"`
 	ID               string      `toml:"id"`
 	Species          string      `toml:"species"`
@@ -19,4 +19,28 @@ type PokemonGift struct {
 	Nickname         string      `toml:"nickname"`
 	LocationOfOrigin string      `toml:"locationOfOrigin"`
 	Shiny            bool        `toml:"shiny"`
+}
+
+type MysteryGiftsServer struct {
+	Mysterygifts []MysteryGiftServer `toml:"mysterygift"`
+}
+
+type PokemonGiftServer struct {
+	OriginalTrainer  string   `toml:"originalTrainer"`
+	ID               int      `toml:"id"`
+	Species          string   `toml:"species"`
+	Nickname         string   `toml:"nickname"`
+	Shiny            bool     `toml:"shiny"`
+	LocationOfOrigin string   `toml:"locationOfOrigin"`
+	Types            []string `toml:"types"`
+	Abilites         []string `toml:"abilites"`
+	Stats            []int    `toml:"stats"`
+}
+
+type MysteryGiftServer struct {
+	Name          string              `toml:"name"`
+	GiftType      string              `toml:"giftType"`
+	BeginningDate string              `toml:"beginningDate"`
+	EndDate       string              `toml:"endDate"`
+	Pokemongift   []PokemonGiftServer `toml:"pokemongift"`
 }
